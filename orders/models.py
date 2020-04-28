4d282360001d9c6323e462fb94c2f7ddb1b91923
Olivia <olivia.wright101@gmail.com>
2020-04-28 11:32:30 +0100

    removed content from orders models (oops) then created products model andd created the table

--- orders/models.py
+++ orders/models.py
@@ -2,15 +2,3 @@ from django.db import models
 from autoslug import AutoSlugField
 
 # Create your models here.
-
-class Product(models.Model):
-    product_name = models.CharField(max_length=255)
-    description = models.TextField(null=True)
-    image_url = models.CharField(max_length=255)
-    price = models.IntegerField()
-    stock_quantity = models.IntegerField()
-    slug = AutoSlugField(populate_from=lambda instance: instance.product_name,
-                         slugify=lambda value: value.replace(' ','-'))
-
-    def __str__(self):
-        return self.product_name
--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from autoslug import AutoSlugField
 
 # Create your models here.
+
+def get_populate_from(instance):
+    return '%s-%s' % (instance.product_name)
+    
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    image_url = models.CharField(max_length=255)
+    price = models.IntegerField()
+    stock_quantity = models.IntegerField()
+    slug = AutoSlugField(populate_from=get_populate_from)
+
+    def __str__(self):
+        return self.product_name
