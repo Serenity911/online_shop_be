678774951175920f8ba08a9d375f2319def27fb0
Serenity911 <silvia.simonassi01@gmail.com>
2020-05-13 17:30:05 +0100

    can add column ordered_price to manytomany table

--- orders/models.py
+++ orders/models.py
@@ -6,9 +6,18 @@ import products.models as products
 class Order(models.Model):
     order_date = models.DateField(auto_now_add = True)
     # customer = models.ForeignKey(customers.Customer, on_delete=models.CASCADE)
-    products = models.ManyToManyField(products.Product)
+    products = models.ManyToManyField(products.Product, related_name='orders', through='Product_Price')
     delivery_address = models.CharField(max_length=200)
     delivery_postcode = models.CharField(max_length=7)
 
     def __str__(self):
         return self.delivery_address
+
+
+class Product_Price(models.Model):
+    product = models.ForeignKey(products.Product, related_name='product_price', on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, related_name='product_price', on_delete=models.CASCADE)
+    # def product_price:
+    #     return product.objects.get()
+
+    ordered_price = models.DecimalField(default='1.00', max_digits=5, decimal_places=2, editable=False)
